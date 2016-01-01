9164fa202e74a63b623fd9965c4fec89a3959587
Pratik Prakash <pratikprakash1@yahoo.com>
2016-01-01 15:28:57 -0600

    Set up the express server and the gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,132 @@
+//Include gulp
+var gulp = require('gulp');
+
+//Include the plugins
+var gutil = require('gulp-util'); //For logging
+var gulpif = require('gulp-if');    
+var streamify = require('gulp-streamify');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var source = require('vinyl-source-stream');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+
+
+var production = process.env.NODE_ENV === 'production';
+
+var dependencies = [
+  'alt',
+  'react',
+  'react-dom',
+  'react-router',
+  'underscore'
+];
+
+/**
+ * Gulp only has 5 methods: task, run, watch, src, dest
+ */
+
+/*
+ |--------------------------------------------------------------------------
+ | Combine all JS libraries into a single file for fewer HTTP requests.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('vendor', function() {
+  return gulp.src([
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/bootstrap/dist/js/bootstrap.js',
+    'bower_components/magnific-popup/dist/jquery.magnific-popup.js',
+    'bower_components/toastr/toastr.js'
+  ]).pipe(concat('vendor.js'))
+    .pipe(gulpif(production, uglify({ mangle: false })))
+    .pipe(gulp.dest('public/js'));
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile third-party dependencies separately for faster performance.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify-vendor', function() {
+  return browserify()
+    .require(dependencies)
+    .bundle()
+    .pipe(source('vendor.bundle.js'))
+    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
+    .pipe(gulp.dest('public/js'));
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile only project files, excluding all third-party dependencies.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify', ['browserify-vendor'], function() {
+  return browserify('app/main.js')
+    .external(dependencies)
+    .transform(babelify, { presets: ['es2015', 'react'] })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
+    .pipe(gulp.dest('public/js'));
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Same as browserify task, but will also watch for changes and re-compile.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify-watch', ['browserify-vendor'], function() {
+  var bundler = watchify(browserify('app/main.js', watchify.args));
+  bundler.external(dependencies);
+  bundler.transform(babelify, { presets: ['es2015', 'react'] })
+  bundler.on('update', rebundle);
+  return rebundle();
+
+  function rebundle() {
+    var start = Date.now();
+    return bundler.bundle()
+      .on('error', function(err) {
+        gutil.log(gutil.colors.red(err.toString()));
+      })
+      .on('end', function() {
+        gutil.log(gutil.colors.green('Finished rebundling in', (Date.now() - start) + 'ms.'));
+      })
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('public/js/'));
+  }
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile LESS stylesheets.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('styles', function() {
+  return gulp.src('app/stylesheets/main.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(gulpif(production, cssmin()))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/stylesheets/**/*.less', ['styles']);
+});
+
+gulp.task('lint', function() {
+   return gulp.src('public/js/*.js').pipe(jshint()).pipe(jshint.reporter('default')); 
+});
+
+//Sets up to complete all the tasks
+gulp.task('default', ['styles', 'vendor', 'lint', 'browserify-watch', 'watch'], function() {
+    console.log("Gulp completed...");
+});
+gulp.task('build', ['styles', 'vendor', 'browserify']);
